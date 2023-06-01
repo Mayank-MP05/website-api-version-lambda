f80b1f492e13aa84aafa49479252e56b9a12035b
Mayank-MP05 <mayank5pande@gmail.com>
2023-06-01 16:29:34 +0530

    bugfix || API route wise data added including social media links

---
+++ data/social-links.js
@@ -0,0 +1,60 @@
+
+const SocialLinksDB = [
+    {
+      name: "Resume",
+      link: "https://drive.google.com/file/d/1o4fcfhSdADrUXuGFrd313IxEnMh62NHk/view?usp=sharing",
+      img: ResumeIcon,
+    },
+  
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/mayank-mp5/",
+      img: LinkedinLogo,
+    },
+    {
+      name: "Github",
+      link: "https://github.com/Mayank-MP05",
+      img: GithubIcon,
+    },
+    {
+      name: "Mail",
+      link: "mailto:mayank5pande@gmail.com",
+      img: GmailIcon,
+    },
+    // 1-4 ends here
+    {
+      name: "Codechef",
+      link: "https://www.codechef.com/users/mayank_mp5",
+      img: CodechefIcon,
+    },
+  
+    {
+      name: "Codeforces",
+      link: "https://codeforces.com/profile/Mayank_MP5",
+      img: CodeforcesIcon,
+    },
+    {
+      name: "Call",
+      link: "tel:8830764295",
+      img: CallPhoneIcon,
+    },
+    {
+      name: "Medium",
+      link: "https://medium.com/@mayank5pande",
+      img: MediumBlogIcon,
+    },
+    // 5-8 Ends here
+    {
+      name: "Devfolio",
+      link: "https://devfolio.co/@mayank_mp5",
+      img: DevfolioIcon,
+    },
+    {
+      name: "Twitter",
+      link: "https://twitter.com/Mayank_MP5",
+      img: TwitterIcon,
+    },
+    // 8-10 here
+  ];
+  
+  export default SocialLinksDB;
---
+++ docs.md
@@ -0,0 +1,18 @@
+Api Wise Routes::
+
+Home/Landing Page -> https://api-version.mayank5pande.com/
+
+/ - list down all the  routes along with some possible description
+
+/projects
+
+/projects/project_slug
+
+
+/performance
+
+/about 
+
+/highlights
+
+/social
