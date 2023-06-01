7eb317b33056ff1eddff8198626cfef351ac4825
Mayank-MP05 <mayank5pande@gmail.com>
2023-06-01 16:05:00 +0530

    feature || Performance metrics data added

--- data/performance-metrics.js
+++ data/performance-metrics.js
@@ -1,3 +1,23 @@
 const performanceMetricsDB = [
-
+    {
+        platform: "Page Speed Insights",
+        scanned_url: "https://mayank-mp05.github.io/#/home",
+        performance_metrics_url: "https://pagespeed.web.dev/analysis/https-mayank-mp05-github-io/swe1ou8yzc?form_factor=mobile",
+    },
+    {
+        platform: "Page Speed Insights",
+        scanned_url: "http://mayank5pande.com/",
+        performance_metrics_url: "https://pagespeed.web.dev/analysis/https-mayank5pande-com/gns0xl4deh?form_factor=mobile",
+    },
+    {
+        platform: "GTmetrix",
+        scanned_url: "http://mayank5pande.com/",
+        performance_metrics_url:
+            "https://gtmetrix.com/reports/mayank5pande.com/kPjLjEuF/",
+    },
+    {
+        platform: "WebPageTest",
+        scanned_url: "http://mayank5pande.com/",
+        performance_metrics_url: "https://www.webpagetest.org/result/230531_AiDcXF_6J8/"
+    }
 ]
