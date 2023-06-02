231efdd885658f69ca7d8ff149e8c852cebbe77a
Mayank-MP05 <mayank5pande@gmail.com>
2023-06-02 14:04:43 +0530

    bugfix || `takeMeBack` url updated in response

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
 console.log('Loading function');
 
+// DOCS: Data imports
+const ProjectsDB = require('./data/projects-list.js');
+const HighlightsDB = require('./data/highlights.js');
+const PerformanceDB = require('./data/performance-metrics.js');
+const AboutDB = require('./data/about-me.js');
+const SocialDB = require('./data/social-links.js');
+
 exports.handler = async (event, context) => {
     const API_BASE_URL = `https://api-version.mayank5pande.com`;
+    let requestType = event?.requestType || 'HOME';
+    const allowedRequestTypes = [
+        'HOME',
+        'PROJECTS',
+        'PERFORMANCE',
+        'ABOUT',
+        'HIGHLIGHTS',
+        'SOCIAL',
+    ]
 
-    return ({
-        url: `${API_BASE_URL}`,
-        projects: `${API_BASE_URL}/projects`,
-        performance: `${API_BASE_URL}/performance`,
-        about: `${API_BASE_URL}/about`,
-        highlights: `${API_BASE_URL}/highlights`,
-        social: `${API_BASE_URL}/social`,
-    });
+    let responseToSend = {}
+
+    if (requestType === 'HOME') {
+        return ({
+            url: `${API_BASE_URL}`,
+            projects: `${API_BASE_URL}/projects`,
+            performance: `${API_BASE_URL}/performance`,
+            about: `${API_BASE_URL}/about`,
+            highlights: `${API_BASE_URL}/highlights`,
+            social: `${API_BASE_URL}/social`,
+        });
+    } else if (requestType === 'PROJECTS') {
+        return ({
+            takeMeBack: `${API_BASE_URL}`,
+            projectsList: ProjectsDB
+        })
+    } else if (requestType === 'PERFORMANCE') {
+        return ({
+            takeMeBack: `${API_BASE_URL}`,
+            performance: PerformanceDB
+        })
+    } else if (requestType === 'ABOUT') {
+        return ({
+            takeMeBack: `${API_BASE_URL}`,
+            about: AboutDB
+        })
+    } else if (requestType === 'HIGHLIGHTS') {
+        return ({
+            takeMeBack: `${API_BASE_URL}`,
+            highlights: HighlightsDB
+        })
+    } else if (requestType === 'SOCIAL') {
+        return ({
+            takeMeBack: `${API_BASE_URL}`,
+            social: SocialDB
+        })
+    }
+
+    return responseToSend;
 };
